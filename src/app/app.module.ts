6144ba6dd6ba5dcb1ad3661d60fe59032d33de52
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2020-07-21 22:47:12 +0300

    Clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -109,28 +109,3 @@ const paths = [
   ]
 })
 export class AppModule {}
-
-/* RESOURCES
-* - https://medium.com/netscape/visualizing-data-with-angular-and-d3-209dde784aeb
-* - https://observablehq.com/@d3/stacked-bar-chart
-* 
-* TODO
-*
-* - Fix analytics
-*
-* - When closing About go to previous screen (and don't destroy component) -- Priority: 7
-* - Click on unanswered question to confirm to go back to questions with the relevant one opened -- Priority: 4
-* - How to treat voter's answer 3 in relation to candidates --- Priority: 5
-* - Implement badge for candidates and opinions that disagree with the party average --- Priority: 5
-* - Tentavive as this may result in problems when zoomed-in, maybe only move the view when zooming out is not necessary. Set zoom nicely when selecting a candidate --- Priority: 3
-* - Tentative: Add candidate avatars on zoom: Grey body, party colour head --- Priority: 5
-* - Add crown to self --- Priority: 6
-* - Add distributions to filters --- Priority: 6
-* - Add exclude missing to range filter --- Priority: 6
-* - Add map-based municipality/constituency choice --- Priority: 6
-* - Save app version in cookie to handle errors --- Priority: 6
-* - Save filters in cookie --- Priority: 6
-* - Enable question prioritizing --- Priority: 7
-* - Allow dynamic language choice (cf Locale specific string functions + base lang in html tag) --- Priority: 9
-* - Convert EventEmitters to Outputs and/or Observables/Subjects --- Priority: 9
-*/
